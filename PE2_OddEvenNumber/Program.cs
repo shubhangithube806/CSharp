a703ff5afc91c7d1a569c39679a15d0c27ec705d
Shubhangi <shubhangithube806@gmail.com>
2024-02-21 22:17:30 +0530

    Extra Exercise - Odd even numbers.

---
+++ PE2_OddEvenNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE2_OddEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Please enter your number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is even number.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is odd number.");
+            }
+        }
+    }
+}
