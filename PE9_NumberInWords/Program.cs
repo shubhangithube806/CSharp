81fef88958aca742067cc082f7ecffc5b8606dfe
Shubhangi <shubhangithube806@gmail.com>
2024-02-23 21:58:16 +0530

    Extra Exercise - NumberInWords

---
+++ PE9_NumberInWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_NumberInWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reminder = 0;
+            string text = "";
+
+            string[] numberText = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+            Console.Write("Enter your number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                reminder = num % 10;
+                text = numberText[reminder] + " " + text;
+                num = num / 10;
+            }
+
+            Console.WriteLine(text);
+           
+        }
+    }
+}
