5611931faa7f9be917e77fa56a7e8016ae6ce051
Shubhangi <shubhangithube806@gmail.com>
2024-02-23 19:00:49 +0530

    Extra Exercise added - FactorialOfNumber

---
+++ PE6_FactorialOfNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE6_FactorialOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, factorial = 1;
+
+            Console.Write("Enter your number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"Factorial of number {num} is {factorial}.");
+        }
+    }
+}
