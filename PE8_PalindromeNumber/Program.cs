e45c6332eb27220ef3a0b7720a8ea2d12a059014
Shubhangi <shubhangithube806@gmail.com>
2024-02-23 19:03:17 +0530

    Extra Exercise Added - PalindromeNumber

---
+++ PE8_PalindromeNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse = 0,reminder, numForOutput;
+
+            Console.Write("Enter your number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            numForOutput = num;
+
+            while (num > 0)
+            {
+                reminder = num % 10;
+                reverse = (reverse * 10) + reminder;
+                num = num / 10;
+            }
+            if (numForOutput == reverse)
+            {
+                Console.WriteLine("This is palindrome number.");
+            }
+            else
+            {
+                Console.WriteLine("This is not palindrome number.");
+            }
+
+
+
+
+
+        }
+    }
+}
