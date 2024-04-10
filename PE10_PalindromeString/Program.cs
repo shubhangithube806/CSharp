05e6202ad5646c4b89278ecebf781fbc559ae97d
Shubhangi <shubhangithube806@gmail.com>
2024-04-10 17:16:21 +0530

    Extra Exercise - Palindrome String

---
+++ PE10_PalindromeString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE10_PalindromeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+
+            Console.Write("Enter your string : ");
+            myString = Console.ReadLine();
+
+            char[] charArray = myString.ToCharArray();
+            Array.Reverse(charArray);
+
+            string reversedString = new string(charArray);
+
+            if (myString == reversedString)
+            {
+                Console.WriteLine($"{myString} is palindrome string.");
+            }
+            else
+            {
+                Console.WriteLine($"{myString} is not palindrome string.");
+            }
+
+        }
+    }
+}
